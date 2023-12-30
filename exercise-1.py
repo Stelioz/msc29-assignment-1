0dcd43490c10719da52b4758ba30f41fa4849fdf
Stylianos Zindros <zindros.stelios@gmail.com>
2023-12-30 12:37:53 +0200

    exercize-1 completed

--- exercise-1.py
+++ exercise-1.py
@@ -21,34 +21,34 @@ def calculate_mse(original, quantized):
 # Στάθμες κβάντισης
 quantization_levels = [7, 11, 15, 19]
 
-# Plot the transformation function for each quantizer
+# Σχεδιασμός συνάρτησης μετασχηματισμού για κάθε κβαντιστή
 plt.figure(figsize=(15, 5))
 for levels in quantization_levels:
     transformation_function_x = np.arange(0, 256, 1)
     transformation_function_y = np.floor_divide(transformation_function_x * levels, 256) * (256 // levels)
-    plt.plot(transformation_function_x, transformation_function_y, label=f'{levels} Levels')
+    plt.plot(transformation_function_x, transformation_function_y, label=f'{levels} Στάθμες')
 
-plt.title('Transformation Functions of Quantizers')
-plt.xlabel('Input Intensity')
-plt.ylabel('Quantized Intensity')
+plt.title('Συναρτήσεις Μετασχηματισμού')
+plt.xlabel('Ένταση Εισόδου')
+plt.ylabel('Κβαντισμένη Ένταση')
 plt.legend()
 plt.show()
 
-# Display the original image and quantized images
+# Εμφάνιση της αρχικής και των κβαντισμένων εικόνων
 plt.figure(figsize=(20, 5))
 
-# Plot the original image
+# Σχεδιασμός της αρχικής εικόνας
 plt.subplot(1, len(quantization_levels) + 1, 1)
 plt.imshow(image, cmap='gray')
-plt.title('Original Image')
+plt.title('Αρχική Εικόνα')
 
-# Plot the quantized images
+# Σχεδιασμός των κβαντισμένων εικόνων
 for i, levels in enumerate(quantization_levels, 1):
     quantized_image = quantize(image, levels)
     mse = calculate_mse(image, quantized_image)
 
     plt.subplot(1, len(quantization_levels) + 1, i + 1)
     plt.imshow(quantized_image, cmap='gray')
-    plt.title(f'{levels} Levels\nMSE: {mse:.2f}')
+    plt.title(f'{levels} Στάθμες\nMSE: {mse:.2f}')
 
 plt.show()
