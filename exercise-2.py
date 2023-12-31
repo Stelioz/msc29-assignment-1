3ad94dbd2e8edba65ad31e31d57b53989c1a39c5
Stylianos Zindros <zindros.stelios@gmail.com>
2023-12-31 11:38:03 +0200

    Minor fixes in exercise-2

--- exercise-2.py
+++ exercise-2.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Φόρτωση εικόνας από τον φάκελο image\
-image = cv2.imread('images/cameraman.bmp', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('images\cameraman.bmp', cv2.IMREAD_GRAYSCALE)
 
 # Υπολογισμός του 2D μετασχηματισμού Fourier (DFT)
 dft = np.fft.fft2(image)
 dft_shift = np.fft.fftshift(dft)
 
 # Υπολογισμός του φάσματος πλάτους και του φάσματος φάσης
-amplitude_spectrum = np.abs(dft)
+magnitude_spectrum = np.abs(dft_shift)
 phase_spectrum = np.angle(dft_shift)
 
 # Εμφάνιση της αρχικής εικόνας, του φάσματος πλάτους και φάσης αυτής
@@ -21,7 +21,7 @@ plt.imshow(image, cmap='gray')
 plt.title('Αρχική Εικόνα')
 
 plt.subplot(1, 3, 2)
-plt.imshow(np.log1p(amplitude_spectrum), cmap='gray')
+plt.imshow(np.log(magnitude_spectrum), cmap='gray')
 plt.title('Φάσμα Πλάτους')
 
 plt.subplot(1, 3, 3)
@@ -37,6 +37,10 @@ mse_values = []
 # Δημιουργία του καμβά του σχήματος
 plt.figure(figsize=(20, 20))
 
+plt.subplot(1, len(percentages) + 1, 1)
+plt.imshow(image, cmap='gray')
+plt.title('Original Image')
+
 for i, percentage in enumerate(percentages, 1):
     # Δημιουργία μάσκας για την αποκοπή των υψηλών συντελεστών
     rows, cols = image.shape
@@ -50,23 +54,17 @@ for i, percentage in enumerate(percentages, 1):
     masked_dft_shift = dft_shift * mask
 
     # Χρήση του αντίστροφου μετασχηματισμού Fourier
-    reconstructed_image = np.abs(np.fft.ifft2(np.fft.ifftshift(masked_dft_shift)))
+    inverse_shifted = np.fft.ifftshift(masked_dft_shift)
+    inverse_result = np.fft.ifft2(inverse_shifted)
+    reconstructed_image = np.abs(inverse_result)
 
     # Υπολογισμός του μέσου τετραγωνικού σφάλματος (MSE)
     mse = np.mean((image - reconstructed_image) ** 2)
     mse_values.append(mse)
 
-    # Εμφάνιση των ανακατασκευασμένων εικόνων, του φάσματος πλάτους και φάσης αυτών
-    plt.subplot(len(percentages), 3, (i - 1) * 3 + 1)
+    # Εμφάνιση των ανακατασκευασμένων εικόνων, του φάσματος πλάτους και φάσης
+    plt.subplot(1, len(percentages) + 1, i + 1)
     plt.imshow(reconstructed_image, cmap='gray')
-    plt.title(f'Εικόνα με {percentage * 100}% των συντελεστών DFT\nMSE: {mse:.2f}')
-
-    plt.subplot(len(percentages), 3, (i - 1) * 3 + 2)
-    plt.imshow(np.log1p(np.abs(masked_dft_shift)), cmap='gray')
-    plt.title('Φάσμα Πλάτους')
-
-    plt.subplot(len(percentages), 3, (i - 1) * 3 + 3)
-    plt.imshow(np.angle(masked_dft_shift), cmap='gray')
-    plt.title('Φάσμα Φάσης')
+    plt.title(f'Εικόνα με {percentage * 100:.0f}% Συντελεστές')
 
 plt.show()
